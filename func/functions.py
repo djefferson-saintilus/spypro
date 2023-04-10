67400728ee8128f729cf11f25f6c59cb2e8aec9f
Djefferson Saintilus <35054290+djefferson-saintilus@users.noreply.github.com>
2023-04-10 18:51:39 -0400

    Add files via upload

--- func/functions.py
+++ func/functions.py
@@ -1,4 +1,4 @@
-import time
+import time,subprocess,os
 # list all functions
 def bannerSpyPro():
 	# banner 
@@ -22,3 +22,39 @@ choose an option from[1-5]:
 5. Reporting
 
 """)
+
+def menuInfo():
+	print("""
+[+] Information Gahtering [+]
+
+choose an option from [1-2]:
+
+1. ping host to detect the OS
+2. basic nmap scan
+""")
+
+def osDiscovery():
+	os.system("clear")
+	bannerSpyPro()
+	print("[+] OS Discovery [+] \n")
+	print("Enter ip/domain")
+	ip=str(input("sp1 > "))
+	time.sleep(1)
+	discoverPingOS=subprocess.run(f'ping -c 1 {ip} | grep "ttl=" | awk -F "ttl=" \'{{print $2}}\' | cut -d " " -f1', shell=True, capture_output=True, text=True)
+	result1 = discoverPingOS.stdout.strip()
+	result1=int(result1)
+	if(result1==64 or result1==63):
+		print("\n === Operating system ====")
+		print('OS : Linux/Unix \n')
+	elif(result1==123 or result1==124):
+		print("\n === Operating system ====")
+		print("\n OS : Windows")
+def basicNmap():
+	os.system("clear")
+	bannerSpyPro()
+	print("[+] Basic Nmap Scan [+] \n")
+	print("Enter ip/domain")
+	ip=str(input("sp1 > "))
+	time.sleep(1)
+	print("\n === Port Scan ====")
+	os.system(f"nmap {ip} -sV -p1-1000 | grep 'open' | sed -E 's/(open)/\\x1b[31m\\1\\x1b[0m/g'")
--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 #coding:utf-8
 #author : Djefferson saintilus
 #spypro v1.0
-import time,subprocess,os
 import func.functions as fc
 
 #Banner
@@ -20,44 +19,20 @@ try:
 		choose=int(input("sp1 > "))
 		if(choose is not int() and choose !=0):
 			if(choose > 0 and choose < 6):
-				os.system("clear")
 				fc.bannerSpyPro()
-				print("""
-	\n [+] Information Gahtering [+]
 
-choose an option from [1-2]:
-
-1. ping host to detect the OS
-2. basic nmap scan
-""")
+				# banner gathering info
+				fc.menuInfo()
+				# end banner
+				
 				choose1=int(input("sp1 > "))
 				# OS discovery
 				if (choose1==1):
-					os.system("clear")
-					fc.bannerSpyPro()
-					print("[+] OS Discovery [+] \n")
-					print("Enter ip/domain")
-					ip=str(input("sp1 > "))
-					time.sleep(1)
-					discoverPingOS=subprocess.run(f'ping -c 1 {ip} | grep "ttl=" | awk -F "ttl=" \'{{print $2}}\' | cut -d " " -f1', shell=True, capture_output=True, text=True)
-					result1 = discoverPingOS.stdout.strip()
-					result1=int(result1)
-					if(result1==64 or result1==63):
-						print("\n === Operating system ====")
-						print('OS : Linux/Unix \n')
-					elif(result1==123 or result1==124):
-						print("\n === Operating system ====")
-						print("\n OS : Windows")
+					fc.osDiscovery()
+
 				# Basic nmap scan
 				elif (choose1==2):
-					os.system("clear")
-					fc.bannerSpyPro()
-					print("[+] Basic Nmap Scan [+] \n")
-					print("Enter ip/domain")
-					ip=str(input("sp1 > "))
-					time.sleep(1)
-					print("\n === Port Scan ====")
-					os.system(f"nmap {ip} -sV -p1-1000 | grep 'open' | sed -E 's/(open)/\\x1b[31m\\1\\x1b[0m/g'")
+					fc.basicNmap()
 				else:
 					print("")
 
